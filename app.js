0c6b79e1394a5d168e57bfc01eddc2c93db39cd9
eduhernandorena <2221524>
2019-02-27 18:56:24 -0300

    WebSocket

--- app.js
+++ app.js
@@ -2,6 +2,19 @@
 var app = require('./config/server');
 
 /* parametrizar a porta de escuta*/
-app.listen(3000, function () {
+var server = app.listen(80, function () {
     console.log('Servidor online');
+});
+
+var io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+/* Criar a conexão por websocket */
+io.on('connection', function (socket) {
+    console.log('Usuário conectou!');
+
+    socket.on('disconnect', function () {
+        console.log("Usuário Desconectou!");
+    });
 });
--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,17 +1,17 @@
-module.exports.iniciaChat = function (application, req, res){
+module.exports.iniciaChat = function (application, req, res) {
     var dadosForm = req.body;
     req.assert('apelido', 'Nome ou apelido é obrigatório!').notEmpty();
-    req.assert('apelido', 'Nome ou apelido deve conter entre 3 e 15 caracteres!').len(3,15);
+    req.assert('apelido', 'Nome ou apelido deve conter entre 3 e 15 caracteres!').len(3, 15);
 
     var erros = req.validationErrors();
 
-    if(erros){
-        res.render('index', {validacao : erros});
+    if (erros) {
+        res.render('index', {validacao: erros});
         //send encerra o processamento... se não tem que ter o 'return;'
         res.send('Existem erros nos formularios!');
-
-
     }
+
+    application.get('io').emit('msgParaCliente', {apelido: dadosForm.apelido, mensagem: ' acabou de entrar no chat!'});
 
     res.render("chat");
 };
